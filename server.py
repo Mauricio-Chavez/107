1b9b8d5a84f17e16f79fc13f9f7c8718d915de62
Mauricio-Chavez <mau_chavez003@hotmail.com>
2024-03-09 14:52:45 -0800

    Class 3 Assigment 3

--- server.py
+++ server.py
@@ -1,15 +1,49 @@
-from flask import Flask
-
+from flask import Flask,request
+import json
 app = Flask(__name__)#__name__ this is using for the name of the folder.
 
 @app.get('/')
 def home():
     return "Hello from flask"
 
-app.run(debug=True)
+@app.get('/testing')
+def testing():
+    return "Hello from another page"
+
+#create 2 more API (about page and blog page)
+
+@app.get('/about')
+def about():
+    me = {"name": "Mauricio"}
+    return json.dumps(me)
+
+@app.get('/blog')
+def blog():
+    return "This is the blog page"
+
+@app.get('/version')
+def version():
+    version = {"name": "mouse", "version": "2", "build": 123456}
+    return json.dumps(version)
+
+# this time we need to read and write in to the server
+products = []
+@app.get('/api/products')
+def get_products():
+    
+    return json.dumps(products)
+
+@app.post('/api/products')
+def save_products():
+    product = request.get_json()
+    print(product)
+    products.append(product)
+    return json.dumps(product)
+app.run(debug=True) # Apply the changes on the code,live 
 
 
-
+# API / Endpoints
+#transform JSON / convert JSON in an object again
 
 
 
